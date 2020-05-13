edaea41e90b7b139b74c3cbc2de7fe1654497949
pabloderivia <htorrespablo@gmail.com>
2020-05-13 22:36:39 +0100

    basic demo of the game

---
+++ Assets/Moneda/Timer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float maxtTime = 60f;
+
+    private float countdown = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = maxtTime;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //deltatime = tiempo que transcurre entre el renderizado de frame y frame
+        countdown -= Time.deltaTime;
+        Debug.Log("tiempo restante: " + countdown);
+
+        if (countdown <= 0)
+        {
+            resetGame();
+        }
+
+    }
+
+
+    void resetGame()
+    {
+
+        Debug.Log("Te has quedado sin tiempo... HAS PERDIDO");
+        coin.coinsCount = 0;
+        SceneManager.LoadScene("MainScene");
+
+    }
+
+    void winGame()
+    {
+
+    }
+}
---
+++ Assets/Moneda/coin.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+
+    public static int coinsCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coin.coinsCount++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") == true)
+        {
+            coinsCount--;
+            Destroy(this.gameObject);
+        }
+
+        if (coin.coinsCount == 0)
+        {
+            Debug.Log("has ganado!!");
+
+            GameObject gameManager = GameObject.Find("GameManager");
+            Destroy(gameManager);
+
+            GameObject[] fireworksSystem = GameObject.FindGameObjectsWithTag("FireWorks");
+
+            foreach (GameObject firework in fireworksSystem)
+            {
+                firework.GetComponent<ParticleSystem>().Play();
+            }
+        }
+    }
+
+}
